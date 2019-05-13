e8189621174b9a8c826d0acbe46065650fbf83a2
Raheel Khan <irahilkhan@gmail.com>
2019-05-13 11:50:51 +0500

    Add files via upload

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -92,19 +92,72 @@ class ListingController extends Controller
     // show single listing 
     public function show($id){
         $listing = Listing::findorFail($id);
+        $areas = ListingArea::where('status', 'active')->with('sector')->get();
         $comments = Comment::where('listing_id', $id)->with('user')->where('status', 'active')->get();
         
-        return view('listing.single')->with('data', $listing)->with('comments', $comments);
+        return view('listing.single')->with('data', $listing)->with('comments', $comments)->with('areas', $areas);
     }
 
     // editing page listing 
     public function edit($id){
-
+        
     }
 
     // update and save the edited listing
     public function update(Request $request){
-
+        $userid = Auth::user()->id;
+        $ad_id = $request->ad_id;
+        $listing = Listing::findOrFail($ad_id);
+        $validated = $request->validate([   
+            "title" => 'required|max:50',
+            "description" => 'required|max:200',
+            "street" => 'max:100',
+            "house" => 'required|max:50',
+            "listing_area_id" => 'required|max:100',
+            "ad_type" => 'required',
+            "price" => 'required|numeric',
+            "pricing_type" => 'required',
+            "contact1" => 'nullable|numeric',
+            "contact2" => 'nullable|numeric',
+            "garages" => 'nullable|numeric',
+            "bedrooms" => 'required|numeric',
+            "washrooms" => 'required|numeric',
+            "siderooms" => 'nullable|numeric',
+            "floors" => 'nullable|numeric',
+            "kitchens" => 'nullable|numeric',
+            "comment" => 'max:150',
+            "description" => 'max:1000',
+            "featured_image" => 'required|mimes:jpeg,jpg,png'
+        ]);
+         if ($request->hasFile('featured_image')) { 
+            $image = $request->file('featured_image'); 
+            $image_name = time().'_'.$image->getClientOriginalName(); 
+            $destinationPath = public_path().'/images'; 
+            $upload = $image->move($destinationPath, $image_name); 
+         }
+        $listing->user_id = $userid;
+        $listing->title = $request->title;
+        $listing->description = $request->description;
+        $listing->street_name = $request->street;
+        $listing->house = $request->house;
+        $listing->listing_area_id = $request->listing_area_id;
+        $listing->type = $request->ad_type;
+        $listing->price = $request->price;
+        $listing->price_type = $request->pricing_type;
+        $listing->contact1 = $request->contact1;
+        $listing->contact2 = $request->contact2;
+        $listing->garages = $request->garages;
+        $listing->bedrooms = $request->bedrooms;
+        $listing->bathroom = $request->washrooms;
+        $listing->siderooms = $request->siderooms;
+        $listing->floors = $request->floors;
+        $listing->kitchens = $request->kitchens;
+        $listing->featured_image = $image_name;
+        $listing->comment = $request->comment;
+        $listing->status = 'active';
+        $listing->save();
+        Session::flash('message', 'Your Ad is Updated. <script>swal("success","Updated","Your Ad is Updated Successfully");</script>'); 
+        return redirect('submit/create');
     }
 
     // destroy or delete the listing
